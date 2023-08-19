ad29a2e3afa871f49a685910b766270e0954e2de
Tân Phạm <tanpham1104@gmail.com>
2023-08-19 17:48:59 +0700

    add Home title

--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -1,5 +1,12 @@
+import Head from '../Head';
+
 const Home = () => {
-  return <main>Home</main>;
+  return (
+    <main>
+      <Head pageTitle="Dashboard" />
+      Home
+    </main>
+  );
 };
 
 export default Home;
