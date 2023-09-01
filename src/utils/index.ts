f2f8126e0b65c0a790a4212d42691b5edbb53e99
Tân Phạm <tanpham1104@gmail.com>
2023-09-01 20:19:33 +0700

    formatDateTime util function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,6 @@
 import { HttpStatusCode } from 'axios';
+import dayjs from 'dayjs';
 
 export const isNotLoginError = (error: any) => error?.response?.status !== HttpStatusCode.Unauthorized;
+
+export const formatDateTime = (timestamp?: Parameters<typeof dayjs>[0]) => dayjs(timestamp).format('MMM D, YYYY');
