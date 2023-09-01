7ab5b6886e48d3cb2ee7780ca671f6af01a5c082
Tân Phạm <tanpham1104@gmail.com>
2023-09-01 20:18:35 +0700

    init tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,14 +8,7 @@ const config: Config = {
     './src/auth/**/*.{js,ts,jsx,tsx,mdx}',
     './src/layouts/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
+  theme: {},
   plugins: [],
 };
 export default config;
